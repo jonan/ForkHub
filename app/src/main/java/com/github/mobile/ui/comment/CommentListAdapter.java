pockethub/PocketHub

Added support to edit / delete issue comments.

app/src/main/java/com/github/mobile/ui/comment/CommentListAdapter.java
@@ -18,6 +18,8 @@ package com.github.mobile.ui.comment;
 import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 import com.github.kevinsawicki.wishlist.SingleTypeAdapter;
 import com.github.mobile.R.id;
@@ -27,6 +29,7 @@ import com.github.mobile.util.HttpImageGetter;
 import com.github.mobile.util.TimeUtils;
 
 import org.eclipse.egit.github.core.Comment;
+import static com.github.mobile.Intents.EXTRA_COMMENT;
 
 /**
  * Adapter for a list of {@link Comment} objects
@@ -38,6 +41,16 @@ public class CommentListAdapter extends SingleTypeAdapter<Comment> {
     private final HttpImageGetter imageGetter;
 
     /**
+     * Callback listener to be invoked when user tries to edit a comment.
+     */
+    private final EditCommentListener editCommentListener;
+
+    /**
+     * Callback listener to be invoked when user tries to edit a comment.
+     */
+    private final DeleteCommentListener deleteCommentListener;
+
+    /**
      * Create list adapter
      *
      * @param inflater
@@ -47,11 +60,7 @@ public class CommentListAdapter extends SingleTypeAdapter<Comment> {
      */
     public CommentListAdapter(LayoutInflater inflater, Comment[] elements,
             AvatarLoader avatars, HttpImageGetter imageGetter) {
-        super(inflater, layout.comment_item);
-
-        this.avatars = avatars;
-        this.imageGetter = imageGetter;
-        setItems(elements);
+        this(inflater, elements, avatars, imageGetter, null, null);
     }
 
     /**
@@ -66,13 +75,59 @@ public class CommentListAdapter extends SingleTypeAdapter<Comment> {
         this(inflater, null, avatars, imageGetter);
     }
 
+    /**
+     * Create list adapter
+     *
+     * @param inflater
+     * @param elements
+     * @param avatars
+     * @param imageGetter
+     */
+    public CommentListAdapter(LayoutInflater inflater, Comment[] elements,
+            AvatarLoader avatars, HttpImageGetter imageGetter,
+            EditCommentListener editCommentListener, DeleteCommentListener deleteCommentListener) {
+        super(inflater, layout.comment_item);
+
+        this.avatars = avatars;
+        this.imageGetter = imageGetter;
+        this.editCommentListener = editCommentListener;
+        this.deleteCommentListener = deleteCommentListener;
+        setItems(elements);
+    }
+
     @Override
-    protected void update(int position, Comment comment) {
+    protected void update(int position, final Comment comment) {
         imageGetter.bind(textView(0), comment.getBodyHtml(), comment.getId());
         avatars.bind(imageView(3), comment.getUser());
 
         setText(1, comment.getUser().getLogin());
         setText(2, TimeUtils.getRelativeTime(comment.getUpdatedAt()));
+
+        // Edit Comment ImageButton
+        final ImageView ivEdit =  view(4);
+        if (editCommentListener != null) {
+            ivEdit.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    editCommentListener.onEditComment(comment);
+                }
+            });
+        } else {
+            ivEdit.setVisibility(View.GONE);
+        }
+
+        // Delete Comment ImageButton
+        final ImageView ivDelete =  view(5);
+        if (deleteCommentListener != null) {
+            ivDelete.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    deleteCommentListener.onDeleteComment(comment);
+                }
+            });
+        } else {
+            ivDelete.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -91,6 +146,6 @@ public class CommentListAdapter extends SingleTypeAdapter<Comment> {
     @Override
     protected int[] getChildViewIds() {
         return new int[] { id.tv_comment_body, id.tv_comment_author,
-                id.tv_comment_date, id.iv_avatar };
+                id.tv_comment_date, id.iv_avatar, id.iv_comment_edit, id.iv_comment_delete };
     }
 }

jonan/ForkHub

app/src/main/java/com/github/mobile/ui/comment/CommentListAdapter.java
@@ -18,7 +18,6 @@ package com.github.mobile.ui.comment;
 
 import android.app.Activity;
 import android.content.res.Resources;
-import android.graphics.Color;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
@@ -47,6 +46,16 @@ public class CommentListAdapter extends MultiTypeAdapter {
     private final HttpImageGetter imageGetter;
 
     /**
+     * Callback listener to be invoked when user tries to edit a comment.
+     */
+     private final EditCommentListener editCommentListener;
+
+    /**
+     * Callback listener to be invoked when user tries to edit a comment.
+     */
+    private final DeleteCommentListener deleteCommentListener;
+
+    /**
      * Create list adapter
      *
      * @param activity
@@ -55,12 +64,7 @@ public class CommentListAdapter extends MultiTypeAdapter {
      * @param imageGetter
      */
     public CommentListAdapter(Activity activity, Comment[] elements, AvatarLoader avatars, HttpImageGetter imageGetter) {
-        super(activity.getLayoutInflater());
-
-        this.resources = activity.getResources();
-        this.avatars = avatars;
-        this.imageGetter = imageGetter;
-        setItems(elements);
+        this(activity, elements, avatars, imageGetter, null, null);
     }
 
     /**
@@ -72,6 +76,27 @@ public class CommentListAdapter extends MultiTypeAdapter {
      */
     public CommentListAdapter(Activity activity, AvatarLoader avatars, HttpImageGetter imageGetter) {
         this(activity, null, avatars, imageGetter);
+    }
+
+    /**
+     * Create list adapter
+     *
+     * @param activity
+     * @param avatars
+     * @param imageGetter
+     * @param editCommentListener
+     * @param deleteCommentListener
+     */
+    public CommentListAdapter(Activity activity, Comment[] elements, AvatarLoader avatars, HttpImageGetter imageGetter,
+            EditCommentListener editCommentListener, DeleteCommentListener deleteCommentListener) {
+        super(activity.getLayoutInflater());
+
+        this.resources = activity.getResources();
+        this.avatars = avatars;
+        this.imageGetter = imageGetter;
+        this.editCommentListener = editCommentListener;
+        this.deleteCommentListener = deleteCommentListener;
+        setItems(elements);
     }
 
     @Override
@@ -183,6 +208,30 @@ public class CommentListAdapter extends MultiTypeAdapter {
 
         setText(1, comment.getUser().getLogin());
         setText(2, TimeUtils.getRelativeTime(comment.getUpdatedAt()));
+
+        // Edit Comment ImageButton
+        if (editCommentListener != null) {
+            imageView(4).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    editCommentListener.onEditComment(comment);
+                }
+            });
+        } else {
+            imageView(4).setVisibility(View.GONE);
+        }
+
+        // Delete Comment ImageButton
+        if (deleteCommentListener != null) {
+            imageView(5).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    deleteCommentListener.onDeleteComment(comment);
+                }
+            });
+        } else {
+            imageView(5).setVisibility(View.GONE);
+        }
     }
 
     public MultiTypeAdapter setItems(Collection<?> items) {
@@ -233,7 +282,7 @@ public class CommentListAdapter extends MultiTypeAdapter {
     protected int[] getChildViewIds(int type) {
         if(type == 0)
             return new int[] { R.id.tv_comment_body, R.id.tv_comment_author,
-                    R.id.tv_comment_date, R.id.iv_avatar };
+                    R.id.tv_comment_date, R.id.iv_avatar, R.id.iv_comment_edit, R.id.iv_comment_delete };
         else
             return new int[]{R.id.tv_event_icon, R.id.tv_event, R.id.iv_avatar};
     }