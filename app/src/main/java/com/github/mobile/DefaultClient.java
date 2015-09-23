38621a6dab58294b68f4dc816ca8869d554689e6
Jon Ander Peñalba <jonan88@gmail.com>
2015-09-23 18:47:31 +0200

    Use OkHttp as HttpClient

--- app/src/main/java/com/github/mobile/DefaultClient.java
+++ app/src/main/java/com/github/mobile/DefaultClient.java
@@ -15,7 +15,14 @@
  */
 package com.github.mobile;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.OkUrlFactory;
+
 import org.eclipse.egit.github.core.client.GitHubClient;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Default client used to communicate with GitHub API
@@ -32,5 +39,19 @@ public class DefaultClient extends GitHubClient {
 
         setSerializeNulls(false);
         setUserAgent(USER_AGENT);
+    }
+
+    /**
+     * Create connection to URI
+     *
+     * @param uri
+     * @return connection
+     * @throws IOException
+     */
+    @Override
+    protected HttpURLConnection createConnection(String uri) throws IOException {
+        OkUrlFactory factory = new OkUrlFactory(new OkHttpClient());
+        URL url = new URL(createUri(uri));
+        return factory.open(url);
     }
 }
