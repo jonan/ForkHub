4ab634bf334e72ad3c3828b6bf968dc1e517a08e
Jon Ander Peñalba <jonan88@gmail.com>
2016-04-03 13:40:29 +0200

    Improve support for user and organization URLs
    New supported URLs:
    https://github.com/stars/jonan
    https://github.com/jonan/followers
    https://github.com/jonan/following
    https://github.com/orgs/github
    https://github.com/orgs/github/people
    https://github.com/orgs/github/teams

--- app/src/main/java/com/github/mobile/core/user/UserUriMatcher.java
+++ app/src/main/java/com/github/mobile/core/user/UserUriMatcher.java
@@ -15,6 +15,12 @@
  */
 package com.github.mobile.core.user;
 
+import static com.github.mobile.ui.user.UserViewActivity.TAB_FOLLOWEES;
+import static com.github.mobile.ui.user.UserViewActivity.TAB_FOLLOWERS;
+import static com.github.mobile.ui.user.UserViewActivity.TAB_MEMBERS;
+import static com.github.mobile.ui.user.UserViewActivity.TAB_STARS;
+import static com.github.mobile.ui.user.UserViewActivity.TAB_TEAMS;
+
 import android.content.Intent;
 
 import com.github.mobile.core.repo.RepositoryUtils;
@@ -36,14 +42,7 @@ public class UserUriMatcher {
      * @return {@link User} or null if path is not valid
      */
     public static User getUser(List<String> pathSegments) {
-        if (pathSegments.size() < 1)
-            return null;
-
-        String login = pathSegments.get(0);
-        if (!RepositoryUtils.isValidOwner(login))
-            return null;
-
-        return new User().setLogin(login);
+        return getUser(pathSegments, 0);
     }
 
     /**
@@ -53,13 +52,74 @@ public class UserUriMatcher {
      * @return {@link Intent} or null if path is not valid
      */
     public static Intent getUserIntent(List<String> pathSegments) {
-        if (pathSegments.size() != 1)
+        if (pathSegments.size() < 1 || pathSegments.size() > 3)
             return null;
 
+        // Organization
+        if (pathSegments.size() == 3) {
+            if (!"orgs".equals(pathSegments.get(0))) {
+                return null;
+            }
+
+            User user = getUser(pathSegments, 1);
+            if (user == null) {
+                return null;
+            }
+
+            switch (pathSegments.get(2)) {
+            case "people":
+                return UserViewActivity.createIntent(user, TAB_MEMBERS);
+            case "teams":
+                return UserViewActivity.createIntent(user, TAB_TEAMS);
+            default:
+                return null;
+            }
+        }
+
+        // User
         User user = getUser(pathSegments);
-        if (user == null)
+
+        if (pathSegments.size() == 1) {
+            if (user == null)
+                return null;
+
+            return UserViewActivity.createIntent(user);
+        }
+
+        if (user != null) {
+            switch (pathSegments.get(1)) {
+            case "followers":
+                return UserViewActivity.createIntent(user, TAB_FOLLOWERS);
+            case "following":
+                return UserViewActivity.createIntent(user, TAB_FOLLOWEES);
+            default:
+                return null;
+            }
+        }
+
+        user = getUser(pathSegments, 1);
+        if (user == null) {
+            return null;
+        }
+
+        switch (pathSegments.get(0)) {
+        case "orgs":
+            return UserViewActivity.createIntent(user);
+        case "stars":
+            return UserViewActivity.createIntent(user, TAB_STARS);
+        default:
+            return null;
+        }
+    }
+
+    public static User getUser(List<String> pathSegments, int position) {
+        if (pathSegments.size() < position + 1)
             return null;
 
-        return UserViewActivity.createIntent(user);
+        String login = pathSegments.get(position);
+        if (!RepositoryUtils.isValidOwner(login))
+            return null;
+
+        return new User().setLogin(login);
     }
 }
