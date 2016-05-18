fee4e8f25bb4a0e226f298ffc3170a00e0f407fc
Jon Ander Peñalba <jonan88@gmail.com>
2016-05-18 19:17:05 +0200

    Add all the API objects needed to fetch the user's notifications

---
+++ app/src/main/java/com/github/mobile/api/model/Subject.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2016 Jon Ander Peñalba
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.mobile.api.model;
+
+public class Subject {
+    public static final String TYPE_ISSUE = "Issue";
+    public static final String TYPE_PULL_REQUEST = "PullRequest";
+    public static final String TYPE_COMMIT = "Commit";
+
+    public String title;
+
+    public String url;
+
+    public String latest_comment_url;
+
+    public String type;
+}
